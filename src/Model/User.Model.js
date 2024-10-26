852a55b0daf324841911d4ec7e7c4754bdf40aa2
Nayeem-codify <webdevlopment2306@gmail.com>
2024-10-27 01:44:48 +0600

    validation

---
+++ src/Model/User.Model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  FirstName: {
+    type: String,
+    required: [true, "Firstname Missing !!"],
+    trim: true,
+  },
+  LastName: {
+    type: String,
+    trim: true,
+  },
+  Email_Address: {
+    type: String,
+    required: [true, "Email Address Missing !!"],
+    unique: true,
+  },
+  Telephone: {
+    type: Number,
+    required: [true, "Telephone Missing !!"],
+    unique: true,
+  },
+});
+
